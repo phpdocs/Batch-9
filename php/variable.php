2d369a164b95334de99aaf1ee685eda10974e263
Mac <mac@Afzal-Mac-Book-Pro-6.local>
2020-06-25 10:10:14 +0500

    Variable and Hello Examples

---
+++ php/variable.php
@@ -0,0 +1,29 @@
+<?php
+    /**
+     * 
+     * Variable Example 
+     * By PHPDocs
+     * https://phpdocs.com/php/variables
+     * 
+     */
+
+     define("BR","<br /><hr />");
+
+     $Name="Afzal";
+     $Org="E-Rozgaar";
+     $BloodGroup="B+";
+     $Height=5.8;
+     $Age=20;
+     $_Salary=500;
+
+    //Print the Variables via Echo
+    echo "Person's Name=\t".$Name.BR;
+    echo "Organization=\t".$Org.BR;
+    echo "Blood Group=\t".$BloodGroup.BR;
+    echo "Height=\t".$Height.BR;
+    echo "Age=\t".$Age.BR;
+    echo "Salary=\t$_Salary".BR;
+
+
+
+?>
